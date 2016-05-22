2c5e05358fa7649b41506da88e96429f0b927a6e
nishizaka <nishizaka@whizzy.co.jp>
2016-05-22 23:52:50 +0900

    enable compile

--- test/run.sh
+++ test/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=/usr/local/lib
+export COMPILER_PATH=/usr/bin
 
 if [ -e gatest.exe ]; then
         rm gatest.exe
 fi
 ############ Compile-force #############################################
 #g++ -g -O0 -lstdc++ ../test/testMain.cpp ../ga/src/Ga.cpp ../ga/src/GaCommon.cpp -o gatest.exe -lm -lgsl -lgslcblas -std=c++11
-g++49 -g -O0 -lstdc++ -I /usr/include/boost148 ../ga/src/SchwefelEllipsoidCMA.cpp -o gatest.exe -lm -lgsl -lgslcblas -std=c++11
+g++48 -Wall -g -O0 ../ga/src/SchwefelEllipsoidCMSA.cpp -o gatest.exe -lm -std=c++11 -lshark
 
 if [ -e gatest.exe ]; then
         ./gatest.exe
