0fdc61f5a3a3aac9b84dc30cc002015e075086a6
nishizaka <nishizaka@whizzy.co.jp>
2016-05-22 17:55:18 +0900

    chage ealib

--- test/run.sh
+++ test/run.sh
@@ -6,8 +6,8 @@ if [ -e gatest.exe ]; then
         rm gatest.exe
 fi
 ############ Compile-force #############################################
-#g++ -lstdc++ ./src/Ga.cpp ./src/GaCommon.cpp -o gatest.exe -lm -lgsl -lgslcblas -std=c++11
-g++ -g -O0 -lstdc++ ../test/testMain.cpp ../ga/src/Ga.cpp ../ga/src/GaCommon.cpp -o gatest.exe -lm -lgsl -lgslcblas -std=c++11
+#g++ -g -O0 -lstdc++ ../test/testMain.cpp ../ga/src/Ga.cpp ../ga/src/GaCommon.cpp -o gatest.exe -lm -lgsl -lgslcblas -std=c++11
+g++49 -g -O0 -lstdc++ -I /usr/include/boost148 ../ga/src/SchwefelEllipsoidCMA.cpp -o gatest.exe -lm -lgsl -lgslcblas -std=c++11
 
 if [ -e gatest.exe ]; then
         ./gatest.exe
