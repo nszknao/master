f32b3222ad4940696250bd6bda23a74bab3c079c
nishizaka <nishizaka@whizzy.co.jp>
2016-05-24 18:53:41 +0900

    remove shark

--- test/run.sh
+++ test/run.sh
@@ -7,7 +7,8 @@ if [ -e gatest.exe ]; then
 fi
 ############ Compile-force #############################################
 #g++ -g -O0 -lstdc++ ../test/testMain.cpp ../ga/src/Ga.cpp ../ga/src/GaCommon.cpp -o gatest.exe -lm -lgsl -lgslcblas -std=c++11
-g++48 -Wall -g -O0 ../ga/src/SchwefelEllipsoidCMSA.cpp -o gatest.exe -lm -std=c++11 -lshark
+#g++48 -Wall -g -O0 ../ga/src/SchwefelEllipsoidCMSA.cpp -o gatest.exe -lm -std=c++11 -lshark
+g++48 -I /usr/include/paradiseo/moeo -I /usr/include/paradiseo/mo -I /usr/include/paradiseo/eo -Wall -g -O0 ../ga/src/Sch1.cpp -o gatest.exe -lm -lcma -leo -leoutils -les -lga -lmoeo
 
 if [ -e gatest.exe ]; then
         ./gatest.exe
