6cf97f621f4ac03342d241848d06a2bde9a13738
nishizaka <nishizaka@whizzy.co.jp>
2016-06-02 17:06:23 +0900

    finish nsga2

--- ga/src/nsga2Sample_Shark.cpp
+++ ga/src/nsga2Sample_Shark.cpp
@@ -3,129 +3,182 @@
 
 #include <Array/Array.h>
 #include <FileUtil/Params.h>
+#include <MOO-EALib/TestFunction.h>
 #include <MOO-EALib/PopulationMOO.h>
 #include <MOO-EALib/ArchiveMOO.h>
 
 using namespace std;
 
-
-inline void calcFitness(IndividualMOO &i)
+void saveArchiveInFile(char *filename, ArchiveMOO &archive)
 {
-    double f1, f2;
-//  ChromosomeT< double >& dblchrom = dynamic_cast< ChromosomeT< double >& >( i[ 0 ] );
+    /********** 後で消す **********/
+    // 境界値
+    double   min = 0;
+    double   max = 1;
+    const Interval rangeOfValues(max, min);
+    unsigned numOfBits   = 20;
+    bool useGrayCode     = true;
+    /********** 後で消す **********/
 
-    f1 = Rng::uni(0, 1);
-    f2 = Rng::uni(0, 1);
+    unsigned no = archive.size();
+    unsigned noOfObj;
+    if (no > 0)
+        noOfObj = archive.readArchive(0).getNoOfObj();
+    else
+        noOfObj = 0;
 
-    i.setMOOFitnessValues(f1, f2);
+    IndividualMOO individual;
+    ChromosomeT< double > chrom;
+
+    double f;
+    ofstream ofs(filename);
+    for (unsigned i = 0; i < no; ++i)
+    {
+        // 目的関数値
+        for (unsigned j = 0; j < noOfObj; ++j)
+        {
+            f   = archive.readArchive(i).getMOOFitness(j);
+            ofs << f << " " << std::flush;
+        }
+
+        ofs << "| ";
+
+        // パラメータ値
+        individual.operator=(archive.readArchive(i));
+        chrom.decodeBinary(individual[0], rangeOfValues, numOfBits, useGrayCode);
+        for (unsigned k = 0; k < chrom.size(); ++k)
+        {
+            ofs << chrom[k] << " " << std::flush;
+        }
+
+        ofs << std::endl;
+    }
+    ofs.close();
 }
 
 // main program
 int main(int argc, char* argv[])
 {
-    unsigned i, ii, t;
+    unsigned i, ii, k, t;
 
     unsigned seed        = 0;
     unsigned dimension   = 10;  // problem dimension
-    unsigned mu          = 100; // population size
-    unsigned iterations  = 100; // maximum number of generations
-    unsigned archiveSize = mu;  // how many solutions in external archive
+    unsigned numOfBits   = 20; 
+    unsigned popSize     = 120; // population size
+    unsigned iterations  = 500; // maximum number of generations
+    bool useGrayCode     = true;
 
-    // bounds
-    double   minInit = -1;
-    double   maxInit = 1;
+    // 境界値
+    double   min = 0;
+    double   max = 1;
 
-    // parameters of NSGA2 mutation and crossover operators
-    double   nm = 20;
-    double   nc = 20;
-    double   pc = 0.9;            // crossover probability
-    double   pm = 1. / dimension; // mutation probability
+    // 突然変異と交叉のパラメータ
+    double   crossProb = 0.9;            // crossover probability
+    double   flipProb = 1. / dimension; // mutation probability
 
-    // initialize random number generator
+    // 変数の定義域を設定
+    // TODO:変数ごとに定義域が異なるものへ対応
+    const Interval rangeOfValues(max, min);
+    double f1 = 0., f2 = 0.;
+
+    // 親個体と子個体
+    double *PF1 = new double[(unsigned)popSize];
+    double *PF2 = new double[(unsigned)popSize];
+    double *OF1 = new double[(unsigned)popSize];
+    double *OF2 = new double[(unsigned)popSize];
+
+    // ランダム値生成器
     Rng::seed(seed);
 
-    // define populations
-    PopulationMOO parents(mu, ChromosomeT< double >(dimension));
-    PopulationMOO offsprings(mu, ChromosomeT< double >(dimension));
+    // 親子の個体群を定義
+    PopulationMOO parents(popSize, ChromosomeT< bool >(dimension*numOfBits));
+    PopulationMOO offsprings(popSize, ChromosomeT< bool >(dimension*numOfBits));
 
-    // set minimization task
+    // 最小化のタスク
     parents   .setMinimize();
     offsprings.setMinimize();
 
-    // set # objective funstions
+    // 目的関数の個数をセット
     parents   .setNoOfObj(2);
     offsprings.setNoOfObj(2);
 
-    // init archive
-    ArchiveMOO archive(archiveSize);
-    archive.minimize();
-    archive.setMaxArchive(archiveSize);
+    // 遺伝子型を表現型にデコードした時の一時保存用
+    ChromosomeT< double > dblchrom;
 
-    // bounds used by operators
-    vector<double> lower(dimension), upper(dimension);
-    for (i = 0; i < lower.size(); ++i) {
-        lower[i] = minInit;
-        upper[i] = maxInit;
+    // 親個体群の初期化
+    for (i = 0; i < parents.size(); ++i)
+       dynamic_cast< ChromosomeT< bool >& >( parents[ i ][ 0 ] ).initialize();
+        // ((ChromosomeT< double >*)&parents[ i ][ 0 ])->initialize(minInit, maxInit);
+    
+    // 個体群の評価
+    for (i = 0; i < parents.size(); ++i) {
+        dblchrom.decodeBinary(parents[ i ][ 0 ], rangeOfValues, numOfBits, useGrayCode);
+
+        // 目的関数
+        f1 = ZDT4FF1(dblchrom);
+        f2 = ZDT4FF2(dblchrom);
+        
+        parents[ i ].setMOOFitnessValues(f1, f2);
+        PF1[ i ] = f1;
+        PF2[ i ] = f2;
     }
-
-    // initialize all chromosomes of parent population
-    for (i = 0; i < parents.size(); ++i)
-//    dynamic_cast< ChromosomeT< double >& >( parents[ i ][ 0 ] ).initialize( minInit, maxInit);
-        ((ChromosomeT< double >*)&parents[ i ][ 0 ])->initialize(minInit, maxInit);
-
-    // evaluate parents
-    for (i = 0; i < parents.size(); ++i)
-        calcFitness(parents[ i ]);
-
-    parents.crowdedDistance();
 
     // iterate
     for (t = 1; t <= iterations; ++t) {
-        cout << "generation: " << t << endl;
+        cout << "Generation: " << t << endl;
 
-        // copy parents to offsprings
+        // 親個体を子個体にコピー
         offsprings.selectBinaryTournamentMOO(parents);
 
         // recombine by crossing over two parents
         for (i = 0; i < offsprings.size(); i += 2)
-            if (Rng::coinToss(pc))
-                dynamic_cast< ChromosomeT< double >& >(offsprings[i][0]).SBX(dynamic_cast< ChromosomeT< double >& >(offsprings[i+1][0]), lower, upper, nc, .5);
+            if (Rng::coinToss(crossProb))
+                offsprings[ i ][ 0 ].crossoverUniform(offsprings[ i+1 ][ 0 ]);
 
-        // mutate by flipping bits and evaluate objective function
-        for (i = 0; i < offsprings.size(); ++i) {
-            // modify
-            dynamic_cast< ChromosomeT< double >& >(offsprings[i][0]).mutatePolynomial(lower, upper, nm, pm);
+        // flipping bitsによって突然変異
+        for (i = 0; i < offsprings.size(); ++i)
+            dynamic_cast< ChromosomeT< bool >& >(offsprings[ i ][ 0 ]).flip(flipProb);
 
-            // evaluate objective function
-            calcFitness(offsprings[ i ]);
+        // 個体群の評価
+        for (i = 0; i < parents.size(); ++i) {
+            dblchrom.decodeBinary(offsprings[ i ][ 0 ], rangeOfValues, numOfBits, useGrayCode);
+
+            // 目的関数
+            f1 = ZDT4FF1(dblchrom);
+            f2 = ZDT4FF2(dblchrom);
+        
+            offsprings[ i ].setMOOFitnessValues(f1, f2);
+            OF1[ i ] = f1;
+            OF2[ i ] = f2;
         }
 
-        // selection
+        // 選択
         parents.selectCrowdedMuPlusLambda(offsprings);
-
-        // every 10 generations the dominated solutions are removed from the archive
-        if (!(t % 10)) {
-            archive.cleanArchive();
-            for (i = 0; i < parents.size(); i++)
-                archive.addArchive(parents[i]);
-            archive.nonDominatedSolutions();
+        for (k = 0; k < parents.size(); k++) {
+            PF1[ k ] = parents[ k ].getMOOFitness(0);
+            PF2[ k ] = parents[ k ].getMOOFitness(1);
         }
-    }
+    } // 繰り返し
 
     // data output
-    archive.cleanArchive();
-    for (ii = 0; ii < archiveSize; ii++)
+    ArchiveMOO archive(popSize);
+    archive.minimize();
+    for (ii = 0; ii < popSize; ++ii) {
         archive.addArchive(parents[ ii ]);
-
+    }
     archive.nonDominatedSolutions();
 
-    char filename[256];
-    sprintf(filename, "nsga2example-%d.out", seed);
+    char filename[10000];
+    sprintf(filename, "nsga2example-%d.out", 1);
+    saveArchiveInFile(filename, archive);
+
     cout    << "size of the archive: "  << archive.size()
     << ", filename of archive: " << filename << endl << endl;
 
-    // ファイルに目的関数値を書き込む
-    archive.saveArchiveGPT(filename);
+    delete [] PF1;
+    delete [] PF2;
+    delete [] OF1;
+    delete [] OF2;
 
     return EXIT_SUCCESS;
 }
